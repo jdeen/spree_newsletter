7bace175403e311cf70b0da6df34b3ccfc45179a
Ziyan Junaideen <jdeen-solutions@outlook.com>
2015-01-25 21:22:01 +0530

    newsletter controller methdos

--- app/controllers/spree/admin/newsletter/newsletters_controller.rb
+++ app/controllers/spree/admin/newsletter/newsletters_controller.rb
@@ -1,2 +1,21 @@
 class Spree::Admin::Newsletter::NewslettersController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
