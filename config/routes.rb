7280f3250591d856a642bae4e8b9acb25169bba1
Ziyan Junaideen <jdeen-solutions@outlook.com>
2015-01-25 14:58:06 +0530

    newsletter dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Spree::Core::Engine.routes.draw do
   post '/newsletter/subscribe' => 'newsletter#store', :as => :newsletter_store
+
+  get '/admin/newsletter' => 'newsletter/dashboard#show', :as => :newsletter_dashboard
 end
